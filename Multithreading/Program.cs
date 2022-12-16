0ae8e1fa439e6ee966f3627d731ff8b4c078cd65
BhumikaVyas-Advanced <Bhumika.Vyas@oneadvanced.com>
2022-12-16 14:14:38 +0530

    Add project files.

---
+++ Multithreading/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //creating thread 1 and 2 to run simulatenously
+
+            Thread t1 = new Thread(ThreadFunc1);
+            Thread t2 = new Thread(ThreadFunc2);
+
+            t1.Start();
+            t2.Start();
+        }
+
+        public static void ThreadFunc1()
+        {
+           
+            /*for (int i =0; i< 100; i++)
+            {
+                Console.WriteLine("Thread 1 : {0}", i);
+                if(i == 5)
+                {
+                    Thread.Sleep(15000);
+                }
+            }*/
+
+        }
+        public static void ThreadFunc2()
+        {
+            
+            /*for (int i =0; i< 10; i++)
+            {
+                Console.WriteLine("Thread 2 : {0}", i);
+               
+            }*/
+
+        }
+    }
+}
